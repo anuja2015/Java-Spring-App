e1e94f7017b8752ea1e3891a5f07eb696a632b30
anuja2015 <armdevu15@gmail.com>
2025-02-14 12:38:38 +0530

    java source code

---
+++ src/main/java/com/canara/app/App.java
@@ -0,0 +1,19 @@
+package com.canara.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static final String MESSAGE = "Hello from Canara Engg College!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
