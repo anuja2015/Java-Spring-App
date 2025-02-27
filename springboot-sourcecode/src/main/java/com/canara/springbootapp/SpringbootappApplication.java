a94eac1e76a117c76c9687fd06ea8e3a02f68179
anuja2015 <armdevu15@gmail.com>
2025-02-27 09:41:50 +0530

    Update SpringbootappApplication.java

--- springboot-sourcecode/src/main/java/com/canara/springbootapp/SpringbootappApplication.java
+++ springboot-sourcecode/src/main/java/com/canara/springbootapp/SpringbootappApplication.java
@@ -1,4 +1,4 @@
-package com.canara.springbootapp;
+package com.project1.springbootapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
